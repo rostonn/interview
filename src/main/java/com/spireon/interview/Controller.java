88d3286ea8741e5ebe11932297f27fea9256b268
Noah Roston <nroston@spireon.com>
2021-05-21 13:43:47 -0400

    added controller

---
+++ src/main/java/com/spireon/interview/Controller.java
@@ -0,0 +1,13 @@
+package com.spireon.interview;
+
+public class Controller {
+
+//    create endpoint to call find difference function
+//    POST /find-difference
+//    {
+//      "arr1": [1,2,3],
+//      "arr2": [2,3]
+//     }
+
+//    returns 1
+}
